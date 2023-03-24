bc292900ebd0e403aea52b49a80b43f1e0cbe4a6
krasnogurskiy <andrey.kranogurskiy@gmail.com>
2023-03-24 02:30:03 +0200

    Add project files.

---
+++ Flower.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace events_flowers
+{
+    public class Sun
+    {
+        public event EventHandler<SunEventArgs> SunRise;
+        public event EventHandler<SunEventArgs> SunSet;
+
+        public void Rise()
+        {
+            Console.WriteLine("Sun rises.");
+            OnSunRise(new SunEventArgs());
+        }
+
+        public void Set()
+        {
+            Console.WriteLine("Sun sets.");
+            OnSunSet(new SunEventArgs());
+        }
+
+        protected virtual void OnSunRise(SunEventArgs e)
+        {
+            SunRise?.Invoke(this, e);
+        }
+
+        protected virtual void OnSunSet(SunEventArgs e)
+        {
+            SunSet?.Invoke(this, e);
+        }
+    }
+
+    public class SunEventArgs : EventArgs { }
+
+    public class Flower
+    {
+        public FlowerType Type { get; set; }
+        public int BloomDuration { get; set; }
+
+        public Flower(FlowerType type, int bloomDuration)
+        {
+            Type = type;
+            BloomDuration = bloomDuration;
+        }
+
+        public void OnSunRise(object sender, SunEventArgs e)
+        {
+            switch (Type)
+            {
+                case FlowerType.Day:
+                    Console.WriteLine($"{Type} flower opens.");
+                    break;
+                case FlowerType.Night:
+                    Console.WriteLine($"{Type} flower closes.");
+                    break;
+            }
+        }
+
+        public void OnSunSet(object sender, SunEventArgs e)
+        {
+            switch (Type)
+            {
+                case FlowerType.Day:
+                    Console.WriteLine($"{Type} flower closes.");
+                    break;
+                case FlowerType.Night:
+                    Console.WriteLine($"{Type} flower opens.");
+                    break;
+            }
+        }
+    }
+
+    public enum FlowerType { Day, Night }
+
+    public class Bee
+    {
+        public void VisitFlower(Flower flower)
+        {
+            Console.WriteLine($"Bee visits {flower.Type} flower.");
+        }
+    }
+
+    public class Girl
+    {
+        public void TakeSelfie(Flower flower)
+        {
+            Console.WriteLine($"Girl takes selfie with {flower.Type} flower.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace events_flowers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Створення об'єктів
+            Sun sun = new Sun();
+            Flower dayFlower = new Flower(FlowerType.Day, 7);
+            Flower nightFlower = new Flower(FlowerType.Night, 10);
+
+            Bee bee = new Bee();
+            Girl girl = new Girl();
+
+            sun.SunRise += dayFlower.OnSunRise;
+            sun.SunRise += nightFlower.OnSunRise;
+            sun.SunRise += (sender, e) =>
+            {
+                bee.VisitFlower(dayFlower);
+                girl.TakeSelfie(dayFlower);
+            };
+            sun.SunSet += dayFlower.OnSunSet;
+            sun.SunSet += nightFlower.OnSunSet;
+            sun.SunSet += (sender, e) =>
+            {
+                bee.VisitFlower(nightFlower);
+                girl.TakeSelfie(nightFlower);
+            };
+
+            for (int i = 1; i <= 30; i++)
+            {
+                Console.WriteLine($"Day {i}:");
+                sun.Rise();
+                Thread.Sleep(1000);
+                sun.Set();
+                Thread.Sleep(1000);
+                Console.WriteLine('\n');
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
